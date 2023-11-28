59bac44e0365d4383d2c2e6bd685e3b519d8f5ba
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2023-11-27 23:01:02 -0300

    test: create test for validade adocao

--- src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -16,6 +16,8 @@ import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,6 +37,16 @@ class AdocaoServiceTest {
 
     @Mock
     private TutorRepository tutorRepository;
+
+    @Spy
+    private List<ValidacaoSolicitacaoAdocao> validacaoSolicitacaoAdocaos = new ArrayList<>();
+
+
+    @Mock
+    private ValidacaoSolicitacaoAdocao validacaoSolicitacaoAdocao1;
+    @Mock
+    private ValidacaoSolicitacaoAdocao validacaoSolicitacaoAdocao2;
+
 
     @Mock
     private EmailService emailService;
@@ -72,5 +84,26 @@ class AdocaoServiceTest {
         assertEquals(pet, adocaoSalva.getPet());
         assertEquals(tutor, adocaoSalva.getTutor());
         assertEquals(solicitacaoAdocaoDto.motivo(), adocaoSalva.getMotivo());
+    }
+
+    @Test
+    @DisplayName("Deve verificar se os validadores são chamados")
+    public void deveVerificarSeOsValidadoresSãoChamados() {
+        //arrange
+        given(petRepository.getReferenceById(Mockito.anyLong())).willReturn(pet);
+        given(tutorRepository.getReferenceById(Mockito.anyLong())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
+        this.solicitacaoAdocaoDto = new SolicitacaoAdocaoDto(10L, 20L, "Motivo qualquer");
+        //act
+        adocaoService.solicitar(solicitacaoAdocaoDto);
+        validacaoSolicitacaoAdocaos.addAll(Arrays.asList(validacaoSolicitacaoAdocao1, validacaoSolicitacaoAdocao2));
+        //assert
+        then(repository).should().save(adocaoArgumentCaptor.capture());
+        Adocao adocaoSalva = adocaoArgumentCaptor.getValue();
+
+        BDDMockito.then(validacaoSolicitacaoAdocao1).should().validar(this.solicitacaoAdocaoDto);
+        BDDMockito.then(validacaoSolicitacaoAdocao2).should().validar(this.solicitacaoAdocaoDto);
+
+
     }
 }
