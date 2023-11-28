ae1fe489c3c0f4cf77af2b577153eb5dac178f9f
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2023-11-27 22:38:32 -0300

    test: create test for save adocao pet

---
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -0,0 +1,76 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class AdocaoServiceTest {
+    @InjectMocks
+    private AdocaoService adocaoService;
+    @Mock
+    private AdocaoRepository repository;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private TutorRepository tutorRepository;
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private List<ValidacaoSolicitacaoAdocao> validacoes;
+
+
+    private SolicitacaoAdocaoDto solicitacaoAdocaoDto;
+
+    @Mock
+    private Pet pet;
+    @Mock
+    private Abrigo abrigo;
+    @Mock
+    private Tutor tutor;
+    @Captor
+    private ArgumentCaptor<Adocao> adocaoArgumentCaptor;
+
+    @Test
+    @DisplayName("Deve salvar um dado no banco de dados")
+    public void deveSalvarDadoNoDB() {
+        //arrange
+        given(petRepository.getReferenceById(Mockito.anyLong())).willReturn(pet);
+        given(tutorRepository.getReferenceById(Mockito.anyLong())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
+        this.solicitacaoAdocaoDto = new SolicitacaoAdocaoDto(10L, 20L, "Motivo qualquer");
+        //act
+        adocaoService.solicitar(solicitacaoAdocaoDto);
+
+        //assert
+        then(repository).should().save(adocaoArgumentCaptor.capture());
+        Adocao adocaoSalva = adocaoArgumentCaptor.getValue();
+
+        assertEquals(pet, adocaoSalva.getPet());
+        assertEquals(tutor, adocaoSalva.getTutor());
+        assertEquals(solicitacaoAdocaoDto.motivo(), adocaoSalva.getMotivo());
+    }
+}
